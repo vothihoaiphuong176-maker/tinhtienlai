ededc22d4076febe43ba83bcd4987777e0a4a680
vothihoaiphuong176-maker <vothihoaiphuong176@gmail.com>
2026-06-17 16:57:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-st.image("logo.jpg")
+st.image("logo.jpg.jpg")
 # Tiêu đề app
-st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_TS. VŨ ĐỨC BÌNH")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 3_VÕ THỊ HOÀI PHUONG")
 
 # Nhập dữ liệu
 C = st.number_input(
